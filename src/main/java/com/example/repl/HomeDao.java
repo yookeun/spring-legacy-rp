d91787bbf795da1a9b76d33416d5348f08c4adf5
= <knoc720@gmail.com>
2017-04-14 16:09:14 +0900

    리플리케이션 코드적용

--- src/main/java/com/example/repl/HomeDao.java
+++ src/main/java/com/example/repl/HomeDao.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public class HomeDao {
 	@Autowired
 	private SqlSession sqlSession;
 	
+	@Transactional(readOnly=true)
 	public List<TestTb> selectList() {
 		return sqlSession.selectList("testMapper.selectList");
 	}
---
+++ src/main/java/com/example/repl/config/ReplicationRoutingDataSource.java
@@ -0,0 +1,16 @@
+package com.example.repl.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+public class ReplicationRoutingDataSource  extends AbstractRoutingDataSource {
+	private Logger log = LoggerFactory.getLogger(ReplicationRoutingDataSource.class);
+    @Override
+    protected Object determineCurrentLookupKey() {
+        String dataSourceType = TransactionSynchronizationManager.isCurrentTransactionReadOnly() ? "read" : "write";
+        log.info("current dataSourceType : {}", dataSourceType);
+        return dataSourceType;
+    }
+}
